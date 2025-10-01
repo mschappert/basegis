e72089a5d8a5e1eb57a8592a473c053f887cd291
Mikayla <92051490+mschappert@users.noreply.github.com>
2025-10-01 15:43:44 -0400

    update

--- NSF2/reproject_and_merge.py
+++ NSF2/reproject_and_merge.py
@@ -2,9 +2,9 @@
 import arcpy
 import os
 
-input_folder = r"C:\Users\mksch\Desktop\SRTM"
-output_folder = r"C:\Users\mksch\Desktop\SRTM\SRTM_P"
-reference_raster = r"D:\typology\data\3_MovingWindow\mw_area\1990_area_1km.tif"
+input_folder = r"C:\Users\mksch\Desktop\ASTER_GDEMv3\ASTGTM_003-20250922_183847" #r"C:\Users\mksch\Desktop\SRTM"
+output_folder = r"C:\Users\mksch\Desktop\ASTER_GDEMv3\Projected_tiles" #r"C:\Users\mksch\Desktop\SRTM\SRTM_P"
+reference_raster = r"C:\Users\mksch\Desktop\SRTM\SRTM_P\mosaic_dem.tif" #r"D:\typology\data\3_MovingWindow\mw_area\1990_area_1km.tif"
 
 # Get spatial reference from reference raster
 spatial_ref = arcpy.Describe(reference_raster).spatialReference
@@ -33,12 +33,12 @@ for raster in rasters:
 arcpy.env.workspace = output_folder
 
 # Output mosaic location and name
-mosaic_output = os.path.join(output_folder, "SRTM_mosaic.tif")
+mosaic_output = os.path.join(output_folder, "GDEM_mosaic.tif") # SRTM mosaic
 
 arcpy.management.MosaicToNewRaster(
     projected_rasters,           # List of projected rasters
     output_folder,               # Output folder
-    "mosaic_dem.tif",           # Output raster name
+    "GDEM_mosaic.tif",           # Output raster name
     spatial_ref,                 # Use reference spatial reference
     "32_BIT_FLOAT",             # Pixel type for DEMs
     "",                         # Cell size (optional)
